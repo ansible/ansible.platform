69284f73db43e2b6990b7a48f427c31cb29ccaa6
Sagar Paul <sagpaul@redhat.com>
2025-07-22 18:45:11 +0530

    Update service.py

--- plugins/modules/service.py
+++ plugins/modules/service.py
@@ -131,7 +131,7 @@ def main():
         order=dict(type="int"),
         state=dict(choices=["present", "absent", "exists", "enforced"], default="present"),
     )
-    test = test
+    test = 'test'
 
     # Create a module with spec
     module = AAPModule(argument_spec=argument_spec, supports_check_mode=True)
