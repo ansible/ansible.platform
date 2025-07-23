2618e09f232462613048cf1603012b68b112879e
KB-perByte <paul.sagar@yahoo.com>
2025-07-23 16:35:52 +0530

    fix filenames

--- tools/scripts/get_aap_gateway_and_dab.py
+++ tools/scripts/get_aap_gateway_and_dab.py
@@ -58,12 +58,12 @@ def _get_requires(pr_body, target):
 
 
 def _checkout_aap_gateway(pr_body):
-    """Checkout aap-gateway, either from devel OR from a specified Pull Request.
+    """Checkout aap-gateway, either from stable-2.5 OR from a specified Pull Request.
        Return the body of the specified Pull Request, if any.
     :param pr_body: The ansible.platform PR body.
     """
     repo_url = "https://github.com/ansible-automation-platform/aap-gateway"
-    branch = "devel"
+    branch = "stable-2.5"
     aap_gateway_pr_body = ""
 
     required_pr = _get_requires(pr_body, target="aap-gateway")
