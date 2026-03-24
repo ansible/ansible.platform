17932871b9543c83f0a15d2a38cdf576f09a4088
komaldesai13 <kodesai@redhat.com>
2026-03-24 13:08:48 +0530

    test edit

--- plugins/modules/application.py
+++ plugins/modules/application.py
@@ -2,7 +2,7 @@
 # coding: utf-8 -*-
 
 # Copyright: (c) 2024, John Westcott <john-westcott-iv>
-# Copyright: (c) 2020,Geoffrey Bachelot <bachelotg@gmail.com>
+# Copyright: (c) 2020,Geoffrey Bachelot <test@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
